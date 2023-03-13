e49942e444cc65668f19b240c933c5f21a7a02c5
Jash Dhinoja <jash.dhinoja@gmail.com>
2023-03-13 13:34:22 -0400

    Added Double tap gesture and reset image offset handler

--- PinchApp/Screens/ContentView.swift
+++ PinchApp/Screens/ContentView.swift
@@ -11,6 +11,14 @@ struct ContentView: View {
     //MARK: Properties
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    func resetImageState(){
+        return withAnimation(.spring()){
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     //MARK: Body
     var body: some View {
@@ -24,6 +32,7 @@ struct ContentView: View {
                     .shadow(color: .black.opacity(0.2), radius: 12,x: 2,y:2)
                     .opacity(isAnimating ? 1: 0)
                     .animation(.linear(duration: 1), value: isAnimating)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                     .onTapGesture(count: 2){
                         if imageScale == 1{
@@ -31,11 +40,24 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         }else{
-                            withAnimation(.spring()){
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     }
+                    .gesture(
+                        DragGesture()
+                            .onChanged{ value in
+                                withAnimation(.linear(duration: 1)){
+                                    imageOffset = value.translation
+                                }
+                                
+                                
+                            }
+                            .onEnded{ _ in
+                                if imageScale <= 1{
+                                    resetImageState()
+                                }
+                            }
+                    )
             }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
