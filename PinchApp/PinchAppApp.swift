1fb33f89cbba6227fc0afaadf4844bc9ac44a89f
Jash Dhinoja <jash.dhinoja@gmail.com>
2023-03-12 20:03:00 -0400

    Initial Commit

---
+++ PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Jash Dhinoja on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
